8eb1f4efc650bebc2f8f3231474a92de56a25aef
PlabonKumarSaha <pkumarsaha71@gmail.com>
2026-02-06 02:10:21 +1100

    add : basic tool call

--- Agent/agent_tool.js
+++ Agent/agent_tool.js
@@ -1,9 +1,21 @@
 import 'dotenv/config';
-import { Agent, run } from '@openai/agents';
+import { Agent, run,tool } from '@openai/agents';
+import { z } from 'zod';
 
+const getWeatherTool = tool({
+ name: 'getWeather',
+ description: 'useful for when you want to get the current weather in a given location',
+ parameters: z.object({
+   location: z.string().describe('name of the city'),
+ }),
+ execute: async ({ city }) => {
+   return `The current weather in ${city} is okay`;
+ },
+})
 const agent = new Agent({
   name: 'Weather agent',
   instructions: 'You are a weather assistant. Provide weather information for a given location.',
+  tools: [getWeatherTool],
 });
 
 async function main(query = '') {
@@ -13,4 +25,4 @@ async function main(query = '') {
   );
   console.log('Result:', result.finalOutput);
 }
-main('I am Plabon. tell me the weather in Narayanganj');
+main('Dhaka');
